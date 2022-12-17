3e3fff75d6faaaca3754df57e9ab8d8b6b1f1df6
brian6932 <github-action@actions-template-sync.noreply.github.com>
2022-12-17 12:10:38 +0000

    chore(template): merge template changes :up:

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "brian6932/dank-scoop:master"
+    [String]$upstream = "<username>/<bucketname>:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
